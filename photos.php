432f2bc2c2382f1679a4b8d90edbc4d2997c17c4
Alan Coiffard <alan.coiffar@gmail.com>
2025-02-28 18:43:10 +0100

    show picture from catalogue

--- photos.php
+++ photos.php
@@ -1,10 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
 
+
 <head>
     <meta charset="UTF-8">
     <meta name="description" content="Découvrez le portfolio d'Alan Coiffard, développeur web et photographe passionné par la nature et la faune sauvage.">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <link rel="icon" href="images/logo.png" width="500px" />
     <title>Portfolio - Alan Coiffard</title>
 
@@ -38,39 +40,26 @@
 <body>
     <?php include("./templates/header.php") ?>
 
-    <section>
+    <!-- Photos Section -->
+    <section id="photos">
 
-        <div class="tags">
-            <button data-tag="portrait">Portrait</button>
-            <button data-tag="noir-et-blanc">Noir et blanc</button>
-            <button data-tag="urbain">Urbain</button>
-        </div>
-        <div class="photos">
-            <div class="photo" data-tags="portrait noir-et-blanc">
-                <img src="./images/hero/hero_image_2.webp" alt="Portrait">
-            </div>
-            <div class="photo" data-tags="urbain">
-                <img src="./images/hero/hero_image_1.webp" alt="Urbain">
-            </div>
+        <h2>Mon Univers Photo - étendue</h2>
+        <div id="bestof" class="photo-grid">
+            <?php
+            $dirname = "./images/catalogue";
+            $images = glob($dirname . "*.webp");
+            foreach ($images as $image) {
+            ?>
+                <div class="photo-item">
+                    <img src="<?= $image ?>" alt="<?= $image ?>">
+                </div>
+            <?php
+            }
+            ?>
         </div>
     </section>
 
     <?php include('./templates/footer.php') ?>
-
-    <script>
-        const buttons = document.querySelectorAll('.tags button');
-        const photos = document.querySelectorAll('.photo');
-
-        buttons.forEach(button => {
-            button.addEventListener('click', () => {
-                const tag = button.getAttribute('data-tag');
-                photos.forEach(photo => {
-                    photo.style.display = photo.getAttribute('data-tags').includes(tag) ? 'block' : 'none';
-                });
-            });
-        });
-    </script>
-
 
 </body>
 
